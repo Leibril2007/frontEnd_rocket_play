17aabc2321aaa3720ed9de7732fd056a70a635cf
Leibril2007 <ariabi.sr@gmail.com>
2025-06-19 22:25:48 -0600

    enviar codigo

--- componentes/vistaProfesor/juegosSeleccion/juegos.js
+++ componentes/vistaProfesor/juegosSeleccion/juegos.js
@@ -11,7 +11,7 @@ function cargarJuegos(){
     secProfesor.className = "sec-profesor";
 
     let titSecJuegProf =  document.createElement('h2');
-    titSecJuegProf.className = "tit-sec-jueg-prof tit-conf-p";
+    titSecJuegProf.className = "tit-sec-jueg-prof tit-conf-p    ";
     titSecJuegProf.textContent = "¿Qué jugaremos hoy?";
     secProfesor.appendChild(titSecJuegProf);
 
@@ -25,7 +25,15 @@ function cargarJuegos(){
     secProfesor.appendChild(titMemP);
 
     divMemProf.addEventListener("click", function(){
-        window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
+
+        if(!this.classList.contains("colorMarcar")){
+            this.classList.add("colorMarcar");
+        } else {
+            this.classList.remove("colorMarcar");
+        }
+
+
+        /* window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html"; */
     })
 
 
@@ -33,7 +41,14 @@ function cargarJuegos(){
     divEmoProf.className = "cuadro-juego div-emo-prof";
     secProfesor.appendChild(divEmoProf);
     divEmoProf.addEventListener("click", function(){
-        window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
+
+        if(!this.classList.contains("colorMarcar")){
+            this.classList.add("colorMarcar");
+        } else {
+            this.classList.remove("colorMarcar");
+        }
+
+        //window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
     })
 
     let titEmoP =  document.createElement('h3');
@@ -46,7 +61,14 @@ function cargarJuegos(){
     divSimonProf.className = "cuadro-juego div-simon-prof";
     secProfesor.appendChild(divSimonProf);
     divSimonProf.addEventListener("click", function(){
-        window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
+
+        if(!this.classList.contains("colorMarcar")){
+            this.classList.add("colorMarcar");
+        } else {
+            this.classList.remove("colorMarcar");
+        }
+
+        //window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
     })
 
     let titSimonP =  document.createElement('h3');
@@ -58,7 +80,14 @@ function cargarJuegos(){
     divMedAmP.className = "cuadro-juego div-med-am-p";
     secProfesor.appendChild(divMedAmP);
     divMedAmP.addEventListener("click", function(){
-        window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
+        
+        if(!this.classList.contains("colorMarcar")){
+            this.classList.add("colorMarcar");
+        } else {
+            this.classList.remove("colorMarcar");
+        } 
+        
+        //window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
     })
 
     let titMedAmP =  document.createElement('h3');
@@ -66,10 +95,81 @@ function cargarJuegos(){
     titMedAmP.textContent = "Recicla y Gana";
     secProfesor.appendChild(titMedAmP);
 
+    /* NIVELES */
+/*     let titNivP =  document.createElement('h2');
+    titNivP.className = "tit-time-p tit-niv-p";
+    titNivP.textContent = "Selecciona el nivel";
+    secProfesor.appendChild(titNivP);
+
+    let dvBaseNivP = document.createElement('div');
+    dvBaseNivP.className = "dv-base-time-ps";
+
+
+    let dvNiv1 =  document.createElement('div');
+    dvNiv1.className = "dv-niv niv1";
+    dvNiv1.textContent = "Nivel 1";
+    dvBaseNivP.appendChild(dvNiv1);
+
+    let dvNiv2 =  document.createElement('div');
+    dvNiv2.className = "dv-niv niv2";
+    dvNiv2.textContent = "Nivel 2";
+    dvBaseNivP.appendChild(dvNiv2);
+
+    let dvNiv3 =  document.createElement('div');
+    dvNiv3.className = "dv-niv niv3";
+    dvNiv3.textContent = "Nivel 3";
+    dvBaseNivP.appendChild(dvNiv3);
+
+    let dvNiv4 =  document.createElement('div');
+    dvNiv4.className = "dv-niv niv4";
+    dvNiv4.textContent = "Nivel 4";
+    dvBaseNivP.appendChild(dvNiv4);
+
+    secProfesor.appendChild(dvBaseNivP); */
+
+
+    /* TIEMPO */
+
 /*     let titTimeP =  document.createElement('h2');
     titTimeP.className = "tit-time-p tit-conf-p";
     titTimeP.textContent = "Tiempo para cada nivel";
-    secProfesor.appendChild(titTimeP); */
+    secProfesor.appendChild(titTimeP);
+
+    let dvBaseTimeP = document.createElement('div');
+    dvBaseTimeP.className = "dv-base-time-ps";
+
+    let dvTime20P =  document.createElement('div');
+    dvTime20P.className = "dv-time veints";
+    dvTime20P.textContent = "20s";
+    dvBaseTimeP.appendChild(dvTime20P);
+
+    let dvTime30P =  document.createElement('div');
+    dvTime30P.className = "dv-time treint";
+    dvTime30P.textContent = "30s";
+    dvBaseTimeP.appendChild(dvTime30P);
+
+    let dvTime40P =  document.createElement('div');
+    dvTime40P.className = "dv-time cuar";
+    dvTime40P.textContent = "40s";
+    dvBaseTimeP.appendChild(dvTime40P);
+
+    let dvTime50P =  document.createElement('div');
+    dvTime50P.className = "dv-time cinc";
+    dvTime50P.textContent = "50s";
+    dvBaseTimeP.appendChild(dvTime50P);
+
+
+    secProfesor.appendChild(dvBaseTimeP); */
+
+    let btnCrearP = document.createElement('div');
+    btnCrearP.className = "btn-crear-p";
+    btnCrearP.textContent = "Crear";
+    secProfesor.appendChild(btnCrearP);
+
+    btnCrearP.addEventListener("click", function(){
+        window.location.href = "../../paginas/paginasProfesor/codigoJuegoProfesor.html";
+    })
+
 
 
     return secProfesor;
