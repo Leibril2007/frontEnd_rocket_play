cb772e6860069b49a7def0b645439ed094989044
Leibril2007 <ariabi.sr@gmail.com>
2025-07-04 17:30:43 -0600

    prueba

---
+++ componentes/conexiones/conexionesProfesor/conexionProfe.js
@@ -0,0 +1,33 @@
+function enviarCodigoJuego(codigoRec, estRec, nombreJuego, nivSel, timeSel){
+
+    fetch('http://localhost:3000/partidas', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            codigo: codigoRec,
+            estado: estRec,
+            juego: nombreJuego,
+            nivel: nivSel,
+            tiempo: timeSel
+        })
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error al guardar el usuario');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Codigo enviado con exito:', data);
+        alert("SE ENVIÓ");
+      })
+      .catch(error => {
+        console.error('Hubo un problema con la solicitud:', error);
+      }); 
+
+
+}
+
+export { enviarCodigoJuego };
