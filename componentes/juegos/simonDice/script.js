90d1ba0cc7007a847b0f2232ad7471e2e5f03e09
Leibril2007 <ariabi.sr@gmail.com>
2025-07-09 20:06:24 -0600

    conexion con trivia

--- componentes/juegos/simonDice/script.js
+++ componentes/juegos/simonDice/script.js
@@ -138,7 +138,7 @@ console.log({
 });
 
 
-  fetch('http://localhost:3000/resultados', {
+  fetch('http://localhost:3000/api/resultadosDania', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
